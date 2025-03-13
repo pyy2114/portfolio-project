8728a0c575e82e9eec003d0583bce0e9c296af43
pyy2114 <pyy2114@naver.com>
2025-03-13 18:03:33 +0900

    레포지토리 개발

--- src/main/kotlin/com/yoon/portfolio/presentation/repository/PresentationRepository.kt
+++ src/main/kotlin/com/yoon/portfolio/presentation/repository/PresentationRepository.kt
@@ -1,7 +1,40 @@
 package com.yoon.portfolio.presentation.repository
 
+import com.yoon.portfolio.domain.entity.*
+import com.yoon.portfolio.domain.repository.*
 import org.springframework.stereotype.Repository
 
 @Repository
-class PresentationRepository {
+class PresentationRepository(
+    private val achievementRepository: AchievementRepository,
+    private val introductionRepository: IntroductionRepository,
+    private val linkRepository: LinkRepository,
+    private val skillRepository: SkillRepository,
+    private val projectRepository: ProjectRepository,
+    private val experienceRepository: ExperienceRepository
+) {
+
+    fun getActiveAchievements(): List<Achievement>{
+        return achievementRepository.findAllByIsActive(true)
+    }
+
+    fun getActiveExperience(): List<Experience>{
+        return experienceRepository.findAllByIsActive(true)
+    }
+
+    fun getActiveIntroduction(): List<Introduction>{
+        return introductionRepository.findAllByIsActive(true)
+    }
+
+    fun getActiveLink(): List<Link>{
+        return linkRepository.findAllByIsActive(true)
+    }
+
+    fun getActiveProject(): List<Project>{
+        return projectRepository.findAllByIsActive(true)
+    }
+
+    fun getActiveSkill(): List<Skill>{
+        return skillRepository.findAllByIsActive(true)
+    }
 }
