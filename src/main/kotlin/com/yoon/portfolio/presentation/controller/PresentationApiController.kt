f0597ef336406667f04d1c9e3465a91961da51ea
pyy2114 <pyy2114@naver.com>
2025-03-14 16:05:05 +0900

    컨트롤러 개발

--- src/main/kotlin/com/yoon/portfolio/presentation/controller/PresentationApiController.kt
+++ src/main/kotlin/com/yoon/portfolio/presentation/controller/PresentationApiController.kt
@@ -1,15 +1,42 @@
 package com.yoon.portfolio.presentation.controller
 
+import com.yoon.portfolio.presentation.dto.IntroductionDTO
+import com.yoon.portfolio.presentation.dto.LinkDTO
+import com.yoon.portfolio.presentation.dto.ProjectDTO
+import com.yoon.portfolio.presentation.dto.ResumeDTO
+import com.yoon.portfolio.presentation.service.PresentationService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api")
-class PresentationApiController {
+@RequestMapping("/api/v1")
+class PresentationApiController(
+    private val presentationService: PresentationService
+) {
 
     @GetMapping("/test")
-    fun test(): String{
+    fun test(): String {
         return "OK"
+    }
+
+    @GetMapping("/introductions")
+    fun getIntroductions(): List<IntroductionDTO> {
+        return presentationService.getIntroductions()
+    }
+
+    @GetMapping("/links")
+    fun getLinks(): List<LinkDTO> {
+        return presentationService.getLinks()
+    }
+
+    @GetMapping("/resume")
+    fun getResume(): ResumeDTO {
+        return presentationService.getResume()
+    }
+
+    @GetMapping("/projects")
+    fun getProjects(): List<ProjectDTO> {
+        return presentationService.getProjects()
     }
 }
--- src/main/kotlin/com/yoon/portfolio/presentation/controller/PresentationViewController.kt
+++ src/main/kotlin/com/yoon/portfolio/presentation/controller/PresentationViewController.kt
@@ -1,13 +1,54 @@
 package com.yoon.portfolio.presentation.controller
 
+import com.yoon.portfolio.domain.constant.SkillType
+import com.yoon.portfolio.presentation.service.PresentationService
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
-class PresentationViewController {
+class PresentationViewController(
+    private val presentationService: PresentationService
+) {
 
     @GetMapping("/test")
     fun test(): String {
         return "test"
     }
+
+    @GetMapping("/")
+    fun index(model: Model): String{
+
+        val introductions = presentationService.getIntroductions()
+        model.addAttribute("introductions", introductions)
+
+        val links = presentationService.getLinks()
+        model.addAttribute("links", links)
+
+        return "presentation/index"
+    }
+
+    @GetMapping("/resume")
+    fun resume(model: Model): String{
+
+        val resume = presentationService.getResume()
+
+        model.addAttribute("resume", resume)
+
+        // 버전 이슈
+        //model.addAttribute("skillTypes", SkillType.values())
+        model.addAttribute("skillTypes", SkillType.entries.toTypedArray())
+
+        return "presentation/resume"
+    }
+
+    @GetMapping("/projects")
+    fun projects(model: Model): String{
+
+        val projects = presentationService.getProjects()
+        model.addAttribute("projects", projects)
+
+        return "presentation/projects"
+    }
+
 }
