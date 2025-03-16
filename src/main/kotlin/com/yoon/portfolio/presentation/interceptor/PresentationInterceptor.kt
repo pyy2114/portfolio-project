1dccaf0214dea67fac7764fa88e332a56b7fdd25
pyy2114 <pyy2114@naver.com>
2025-03-16 21:54:11 +0900

    인터셉터 개발

--- src/main/kotlin/com/yoon/portfolio/presentation/interceptor/PresentationInterceptor.kt
+++ src/main/kotlin/com/yoon/portfolio/presentation/interceptor/PresentationInterceptor.kt
@@ -1,7 +1,24 @@
 package com.yoon.portfolio.presentation.interceptor
 
+import com.yoon.portfolio.domain.entity.HttpInterface
+import com.yoon.portfolio.domain.repository.HttpInterfaceRepository
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
+import java.lang.Exception
 
 @Component
-class PresentationInterceptor {
+class PresentationInterceptor(
+    private val httpInterfaceRepository: HttpInterfaceRepository
+): HandlerInterceptor {
+    override fun afterCompletion(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        handler: Any,
+        ex: Exception?
+    ) {
+        val httpInterface = HttpInterface(request)
+        httpInterfaceRepository.save(httpInterface)
+    }
 }
