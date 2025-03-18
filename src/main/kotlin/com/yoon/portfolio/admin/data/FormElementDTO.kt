6bba8251bb2c208c0240df5f8a89cf23e701d43b
pyy2114 <pyy2114@naver.com>
2025-03-18 16:44:50 +0900

    공통 개발 - DTO

--- src/main/kotlin/com/yoon/portfolio/admin/data/FormElementDTO.kt
+++ src/main/kotlin/com/yoon/portfolio/admin/data/FormElementDTO.kt
@@ -4,5 +4,23 @@ abstract class FormElementDTO(
     val name : String,
     val size : Int,
     val type : String
-) {
+)
+
+// 위의 추상 클래스 구현
+class TextFormElementDTO(
+    name: String,
+    size: Int
+) : FormElementDTO(name = name, size = size, type = "text")
+
+class DateFormElementDTO(
+    name: String,
+    size: Int
+) : FormElementDTO(name = name, size = size, type = "date")
+
+class SelectFormElementDTO(
+    name: String,
+    size: Int,
+    options: List<Any>
+) : FormElementDTO(name = name, size = size, type = "select"){
+    val options: List<Any> = options
 }
